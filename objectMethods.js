3f9544e5fcd8f181853ed9a499b19c2293f10361
Kevin Wong <kkwokwai22@gmail.com>
2017-05-17 15:18:34 -0700

    toString method

--- objectMethods.js
+++ objectMethods.js
@@ -9,3 +9,15 @@
 // isPrototypeOf
 //
 // propertyIsEnumerable
+//
+// As i was researching about the object toString i realize the
+//
+// [] typeof is a object not only that ... it contains array method/object methods
+//
+var a = 1000;
+var stringifyA = a.toString(); // '1000'
+console.log(stringifyA);
+
+var arrayOfFruits = ['apple', 'grapes', 'banana', 'orange'];
+var stringifyArray = arrayOfFruits.toString();
+console.log(stringifyArray);
